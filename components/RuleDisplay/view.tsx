fe6f80f9780ee88b895b3f543f255462c9d079a8
asRagi <ragi-justaway@hotmail.co.jp>
2020-12-08 02:41:24 +0900

    update css

--- components/RuleDisplay/view.tsx
+++ components/RuleDisplay/view.tsx
@@ -11,6 +11,7 @@ export const RuleDisplayView = ({
   timeFrom,
   timeTo,
 }: RuleDisplayViewProps) => {
+  const tilt = Math.random() * 6 - 3;
   return (
     <div className='rule-display-container'>
       <div className='img-container'><img src={ruleImgSrc} /></div>
@@ -26,6 +27,12 @@ export const RuleDisplayView = ({
       <style jsx>{`
         .rule-display-container {
           display: flex;
+          transform: rotate(${tilt}deg);
+          padding: 12px 24px;
+          border-radius: 8px;
+          background: #212121;
+          color: white;
+          font-weight: bold;
         }
 
         .img-container {
